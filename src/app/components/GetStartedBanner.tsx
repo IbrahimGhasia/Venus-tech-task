724ea26aac27f8567a46784f9ac66c3fe51734d1
IbrahimGhasia <ibrahim.ghasia@gmail.com>
2023-09-24 18:34:10 +0530

    fixed margin but

--- src/app/components/GetStartedBanner.tsx
+++ src/app/components/GetStartedBanner.tsx
@@ -3,10 +3,8 @@ import Button from "./UI/Button";
 export default function GetStartedBanner() {
 	return (
 		<div className="flex justify-between items-center bg-gradient-to-r via-violet-100 to-sky-200 from-pink-200 py-28 px-20 my-20">
-			<div>
-				<h1 className="text-_darkBlue font-bold text-5xl">
-					Start your AI jouney with us!
-				</h1>
+			<div className="text-_darkBlue font-bold text-5xl">
+				Start your AI jouney with us!
 			</div>
 
 			<div>
